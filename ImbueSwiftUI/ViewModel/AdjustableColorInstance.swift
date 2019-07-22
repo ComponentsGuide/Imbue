a564879c427fc24b60068e5dab74838bc222f7db
Patrick Smith <pgwsmith@gmail.com>
2019-07-22 16:47:39 +1000

    Add test for view model

---
+++ ImbueSwiftUI/ViewModel/AdjustableColorInstance.swift
@@ -0,0 +1,68 @@
+//
+//  AdjustableColorInstance.swift
+//  ImbueSwiftUI
+//
+//  Created by Patrick Smith on 21/7/19.
+//  Copyright © 2019 Royal Icing. All rights reserved.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+
+class AdjustableColorInstance : BindableObject {
+    let willChange = PassthroughSubject<(), Never>()
+    
+    struct State {
+        var inputColor: ColorValue = ColorValue.labD50(ColorValue.Lab(l: 50, a: 0, b: 0))
+        var lightenAmount: CGFloat = 0.0
+        var darkenAmount: CGFloat = 0.0
+        var desaturateAmount: CGFloat = 0.0
+        var invert: Bool = false
+    }
+    
+    var state: State {
+        willSet {
+            willChange.send()
+        }
+    }
+    
+    init(state: State) {
+        self.state = state;
+    }
+    
+    private enum Step : Int {
+        case lighten = 0
+        case darken
+        case desaturate
+        case invert
+        
+        var index: Int { rawValue }
+    }
+    
+    private func transform(color: ColorValue, upTo: Step) -> ColorValue {
+        var rgb = color.toSRGB()!
+        for step in 0 ... upTo.index {
+            switch step {
+            case Step.lighten.index:
+                rgb = rgb.lightened(amount: state.lightenAmount)
+            case Step.darken.index:
+                rgb = rgb.darkened(amount: state.darkenAmount)
+            case Step.desaturate.index:
+                rgb = rgb.desaturated(amount: state.desaturateAmount)
+            case Step.invert.index:
+                if state.invert {
+                    rgb = rgb.inverted()
+                }
+            default:
+                break
+            }
+        }
+        return ColorValue.sRGB(rgb)
+    }
+    
+    var outputColor: ColorValue {
+        transform(color: state.inputColor, upTo: .invert)
+    }
+}
--- ImbueSwiftUITests/ImbueSwiftUITests.swift
+++ ImbueSwiftUITests/ImbueSwiftUITests.swift
@@ -9,26 +9,35 @@
 import XCTest
 @testable import ImbueSwiftUI
 
-class ImbueSwiftUITests: XCTestCase {
+func Then(_ description: String, _ block: () -> ()) {
+    XCTContext.runActivity(named: "Then \(description)") { _ in
+        block()
+    }
+}
 
+class AdjustableColorInstanceTests: XCTestCase {
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testWhenNoAdjustments() {
+        let a = AdjustableColorInstance(state: .init(inputColor: ColorValue.sRGB(.init(r: 0.25, g: 0.75, b: 0.5))))
+        
+        Then("output color is same as input color") {
+            XCTAssertEqual(a.outputColor, ColorValue.sRGB(.init(r: 0.25, g: 0.75, b: 0.5)))
+        }
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
